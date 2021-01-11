914ece1ff92d7d48dce449a96e664fb839a4793f
PROGR WEB - MANHA <PROGR WEB - MANHA@MININT-U2SA7DV>
2021-01-11 11:59:24 -0300

    projeto do portifólio 11/01

---
+++ mportifolio.php
@@ -0,0 +1,35 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+    <head>
+        <meta charset="UTF-8">
+        <title>Portifólio</title>
+        <link rel="stylesheet" href="plugins/bootstrap/css/bootstrap.min.css"/>
+        <link rel="stylesheet" href="assets/css/style.css"/>
+    </head>
+    <body>
+        <div class="container">
+            <div class="rol">
+                <div class="col bg-dark container-fluid">
+                    <ul class="nav justify-content-center">
+                        <li class="nav-item">
+                            <a class="nav-link active" href="plugins/p1/dist/index.html">Plugin1</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="plugins/p2/dist/index.html">Plugin2</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="plugins/p3/dist/index.html">Plugin3</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link disabled" href="#">Plugin4</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link disabled" href="#">Plugin5</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+    </body>
+</html>
---
+++ plugins/p3/README.markdown
@@ -0,0 +1,5 @@
+# 
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/lucas2213690/pen/GqRqqv](https://codepen.io/lucas2213690/pen/GqRqqv).
+
+
