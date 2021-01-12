dad620c7bb077898863ea96ce6b5ec48a122c5d2
PROGR WEB - MANHA <PROGR WEB - MANHA@MININT-U2SA7DV>
2021-01-12 09:06:30 -0300

    projeto do portifólio 11/01 acrescentei mais dois projetos de amigos.

--- mportifolio.php
+++ mportifolio.php
@@ -9,7 +9,7 @@
     <body>
         <div class="container">
             <div class="rol">
-                <div class="col bg-dark container-fluid">
+                <div class="col bg-dark container-fluid mt-4">
                     <ul class="nav justify-content-center">
                         <li class="nav-item">
                             <a class="nav-link active" href="plugins/p1/dist/index.html">Plugin1</a>
@@ -21,10 +21,10 @@
                             <a class="nav-link" href="plugins/p3/dist/index.html">Plugin3</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link disabled" href="#">Plugin4</a>
+                            <a class="nav-link disabled" href="plugins/p4/dist/index.html">Plugin4</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link disabled" href="#">Plugin5</a>
+                            <a class="nav-link disabled" href="plugins/p5/p1.php">Plugin5</a>
                         </li>
                     </ul>
                 </div>
